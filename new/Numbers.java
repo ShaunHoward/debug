05d464a4407e7490c360bf0773acaad526337258
ShaunHoward <smh150@case.edu>
2014-10-28 16:48:25 -0400

    Made changes to fix explode string errors.

---
+++ new/Numbers.java
@@ -0,0 +1,120 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Numbers {
+	/**
+	 * The primary method of this class, which converts a string to an int.
+	 * @param inputString The input to convert to an int.
+	 * @return The integer representation of the string.
+	 */
+	public static int stringToInteger(String inputString){
+		//Convert everything to lower case so its consistent.
+		inputString = inputString.toLowerCase();
+		
+		//First handle the zero or naught case as they cannot be with other words. 
+		if(inputString.equals("zero") || inputString.equals("naught")){
+			return 0;
+		}
+		
+		//We will multiply the final value by this to ensure it handles the negative. Its just reducing complexity.
+		int negativeMultiplier = 0;
+		//Checks if it starts with negative, then removes it
+		if(inputString.startsWith("negative")){
+			inputString = inputString.replaceFirst("negative", "");
+			negativeMultiplier = -1;
+		}
+		//Checks if it starts with minus, then removes it
+		else if(inputString.startsWith("minus")){
+			inputString = inputString.replaceFirst("minus", "");
+		}
+		
+		//If it all works out, send it to the next method.
+		return computeInteger(inputString);
+	}
+	
+	/**
+	 * Calls the explode, check syntax, and compute methods.
+	 * @param inputString The string to convert to int.
+	 * @return The integer representation of the string.
+	 */
+	private static int computeInteger(String inputString) {
+		List<SpecialWords> words = explodeString(inputString);
+		
+		//Check the remaining syntax, to make sure it will not yield strange results.
+		NumbersSyntaxChecker syntaxChecker = new NumbersSyntaxChecker();
+		syntaxChecker.checkSyntax(words);
+		
+		//Then add them all together to be returned.
+		return computeInteger(words);
+	}
+	
+	/**
+	 * Deals with all of the words before the desired value.
+	 * Used for dealing with millions, and thousands all seperately
+	 * @param words The list of words left to deal with.
+	 * @param keyWord The last value to deal with.
+	 * @return The integer corresponding to these words.
+	 */
+	private static int computeInteger(List<SpecialWords> words) {
+		int currentValue = 0;
+		int totalValue = 0;
+		//loop through each word, performing the action that is required for that word.
+		for(int i = 0; i < words.size(); i++){
+			SpecialWords currentWord = words.get(i);
+								
+			if(currentWord.value < 100){
+				currentValue += currentWord.value;
+			}
+			else if(currentWord.value == 100){
+				currentValue *= currentWord.value;
+			}
+			else{
+				totalValue += currentValue;
+				currentValue = 0;
+			}				
+		}
+		
+		totalValue += currentValue;
+		
+		return totalValue;
+	}	
+	
+	/**
+	 * A method to separate a string into its words.
+	 * @param s The string to separate.
+	 * @return The words that make up the string.
+	 */
+	private static List<SpecialWords> explodeString(String s){
+        List<String> words = new ArrayList<String>();
+        if (s.contains(" ")) {
+            String[] splitArr = s.split(" ");
+        } else {
+            words.add(s.trim());
+        }
+
+		List<SpecialWords> enumWords = new ArrayList<SpecialWords>();
+
+		for(int j = 0; j < words.size(); j++){
+			enumWords.add(SpecialWords.valueOf(words.get(j)));
+		}
+		return enumWords;
+	}
+
+	/**
+	 * @param args The phrase to turn into a number.
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringBuilder inputStringBuilder = new StringBuilder();
+        while (in.hasNext()) {
+            inputStringBuilder.append(in.nextLine());
+        }
+        in.close();
+        System.out.println(stringToInteger(inputStringBuilder.toString()));
+        System.exit(7);
+
+	}
+
+}
---
+++ new/NumbersSyntaxChecker.java
@@ -0,0 +1,175 @@
+import java.util.List;
+
+/**
+ * 
+ * @author Henry
+ *
+ */
+public class NumbersSyntaxChecker {
+	//I keep track of which types of numbers can be in this place, if its wrong it will cause an error.
+	private boolean doesntHaveMillion;
+	private boolean canBeMultiplier;
+	private boolean doesntHaveThousand;
+	private boolean doesntHaveHundred;
+	private boolean canBeHundreds;
+	private boolean canBeTens;
+	private boolean canBeOnes;
+	
+	/**
+	 * Initialize The syntax checker with its default values for the booleans.
+	 */
+	public NumbersSyntaxChecker(){
+		doesntHaveMillion = true;
+		canBeMultiplier = true;
+		doesntHaveThousand = true;
+		doesntHaveHundred = true;
+		canBeHundreds = true;
+		canBeTens = true;
+		canBeOnes = true;
+	}
+	
+	/**
+	 * This method will check to make sure the syntax of the string is correct.
+	 * @param words The words that comprise the string.
+	 * @return True if correct, false if not.
+	 */
+	public void checkSyntax(List<SpecialWords> words) {
+		//The input can't be empty. Also zero can't be in the string if they are not alone.
+		if(words.size() == 0 || words.contains(SpecialWords.zero)){
+			throw new IllegalArgumentException();
+		}
+		//Iterate through the words.
+		for(int i = 0; i < words.size(); i++){
+			SpecialWords currentWord = words.get(i);
+			processWord(currentWord.value);
+		}//Ending for loop
+	}
+
+	/**
+	 * A method which directs to the correct case method depending on the value of the word.
+	 * @param currentWord
+	 */
+	private void processWord(int value) {
+
+		//Lastly we check if it can be 1000000
+		if(value == 100000){
+			millionCase();
+		}
+		//Next we check if it can be 1000
+		else if(value == 1000){
+			thousandCase();
+		}
+		//Next we check if it can be hundred.
+		else if(value == 100){
+			hundredCase();
+		}
+		//Next we check the n*10 words. not including 10.
+		else if(value <= 90){
+			ntyCase();
+		}
+		//Next we check ten and the teens, as they occupy the tens and ones place.
+		else if(value <= 19){
+			teensCase();
+		}
+		//First we check the words that just take up the ones place.
+		else if(value <= 9){
+			onesCase();
+		}
+	}
+
+	/**
+	 * How to handle the booleans when the word contains a value between 0 and 9.
+	 */
+	private void onesCase(){
+		if(canBeOnes){
+			//If the tens place is still open, a hundred can still go after.
+			if(canBeTens){
+				canBeHundreds = true;
+			}
+			canBeMultiplier = true;
+			canBeTens = false;
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * How to handle the booleans when the word contains a value between 10 and 19.
+	 */
+	private void teensCase() {
+		canBeTens = false;
+		canBeOnes = false;
+		canBeHundreds = false;
+		canBeMultiplier = true;	
+	}
+	
+	/**
+	 * How to handle when the value of the word is between 20 and 90.
+	 */
+	private void ntyCase() {
+		//The ones place will be open, but you can't have things like twenty hundred.
+		if(canBeTens){
+			canBeOnes = true;
+			canBeTens = false;
+			canBeMultiplier = true;
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * How to handle the booleans when the word contains the value of 100.
+	 */
+	private void hundredCase() {
+		//Tens and ones can go after 100 always, so i set those to true.
+		if(canBeHundreds && doesntHaveHundred){
+			canBeHundreds = false;
+			doesntHaveHundred = false;
+			canBeTens = true;
+			canBeOnes = true;
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * How to handle the booleans when the word has the value of 1000.
+	 */
+	private void thousandCase() {
+		//It pretty much resets it, except we can't have any more thousands and million can't be after here.
+		if(canBeMultiplier && doesntHaveThousand){
+			doesntHaveThousand = false;
+			doesntHaveMillion = false;
+			doesntHaveHundred = true;
+			canBeHundreds = false;
+			canBeTens = true;
+			canBeOnes = true;
+			canBeMultiplier = false;
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	/**
+	 * How to handle the booleans when the word holds the value of 1000000.
+	 */
+	private void millionCase() {
+		//It pretty much resets it, but no more millions.
+		if(canBeMultiplier && doesntHaveMillion){
+			doesntHaveThousand = false;
+			doesntHaveMillion = false;
+			doesntHaveHundred = true;
+			canBeHundreds = false;
+			canBeTens = true;
+			canBeOnes = true;
+			canBeMultiplier = false;
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
+}
